dbdb22f937faf21f5a15a5db11bfd4bfd2354237
Dikshita Choudhary <dikshitachoudhary845@gmail.com>
2026-06-20 22:04:55 +0530

    Create Leetcode_que1.c

---
+++ DSA/Dikshita_Choudhary_590026654 /Day_6/Leetcode_que1.c
@@ -0,0 +1,22 @@
+int robRange(int* nums, int start, int end) {
+    int prev1 = 0;
+    int prev2 = 0;
+
+    for (int i = start; i <= end; i++) {
+        int temp = prev1;
+        prev1 = (prev2 + nums[i] > prev1) ? prev2 + nums[i] : prev1;
+        prev2 = temp;
+    }
+
+    return prev1;
+}
+
+int rob(int* nums, int numsSize) {
+    if (numsSize == 1)
+        return nums[0];
+
+    int option1 = robRange(nums, 0, numsSize - 2);
+    int option2 = robRange(nums, 1, numsSize - 1);
+
+    return (option1 > option2) ? option1 : option2;
+}
