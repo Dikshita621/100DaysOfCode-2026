5b83d9a40068df4cb09e2cc7f94a6262e5bd1eb6
Dikshita Choudhary <dikshitachoudhary845@gmail.com>
2026-06-23 19:48:38 +0530

    Add function to find length of last word in string

---
+++ DSA/Dikshita_Choudhary_590026654 /Day_9/leetcode_que1.c
@@ -0,0 +1,15 @@
+#include <string.h>
+
+int lengthOfLastWord(char* s) {
+    int i = strlen(s) - 1;
+    int len = 0;
+
+    while (i >= 0 && s[i] == ' ') {
+        i--;
+    }
+    while (i >= 0 && s[i] != ' ') {
+        len++;
+        i--;
+    }
+    return len;
+}
